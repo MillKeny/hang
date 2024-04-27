9d5658dd8fc3406f4560dc4174532242567e201f
Artyom <102876334+MillKeny@users.noreply.github.com>
2024-04-27 14:19:55 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const bd = document.body;
 const bdctx = bd.innerHTML;
 const template = `
-<h1 style="font-family: 'Lucida Console', 'Courier New', monospace; text-shadow: 0px 3px 2px rgb(91, 100, 110); font-weight: 1000; color:rgb(30, 30, 30);">MillKeny-ի Անիմաստ Հայերեն Բառարանի Ծրագրեր (անճաշակ դիզայնով)</h1>
+<h1 style="font-family: 'Lucida Console', 'Courier New', monospace; text-shadow: 0px 3px 4px rgb(91, 100, 110); font-weight: 1000; color:rgb(30, 30, 30);">MillKeny-ի Անիմաստ Հայերեն Բառարանի Ծրագրեր (անճաշակ դիզայնով)</h1>
 <button class="hdr" onclick="location.href='index.html'">Հանգեր</button>
 <button class="hdr" onclick="location.href='mostfreq.html'">Ամենատարածվածներ</button>
 <button class="hdr" onclick="location.href='writer.html'">Պոետ</button>
@@ -13,5 +13,3 @@ ${bdctx}
 
 
 bd.innerHTML = template;
-
-console.log(template)
