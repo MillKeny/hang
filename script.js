306b675e22ff3dadede803d5a4ddfb24675379ae
Artyom <102876334+MillKeny@users.noreply.github.com>
2024-08-05 19:42:10 +0400

    Design Update

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const bd = document.body;
-const bdctx = bd.innerHTML;
-const template = `
-<h1 style="font-family: 'Lucida Console', 'Courier New', monospace; text-shadow: 0px 3px 4px rgb(91, 100, 110); font-weight: 1000; color:rgb(30, 30, 30);">MillKeny-ի Անիմաստ Հայերեն Բառարանի Ծրագրեր (անճաշակ դիզայնով)</h1>
-<button class="hdr" onclick="location.href='index.html'">Հանգեր</button>
-<button class="hdr" onclick="location.href='mostfreq.html'">Ամենատարածվածներ</button>
-<button class="hdr" onclick="location.href='writer.html'">Պոետ</button>
-<button class="hdr" onclick="location.href='xach.html'">ԽԱՉԲԱՌ</button>
-<button class="hdr" onclick="location.href='dic.html'">Որոնում</button>
-<button class="hdr" onclick="location.href='wordle.html'">WORDLE</button>
-${bdctx}
-`;
+window.onload = function() {
+    const bd = document.body;
+    const bdctx = bd.innerHTML;
+    const template = `
+    <img src="logo.png" class="logo" >
+    <div style="text-align: center; padding-bottom: 15px;">
+    <button class="hdr" onclick="location.href='index.html'">Հանգեր</button>
+    <button class="hdr" onclick="location.href='mostfreq.html'">Ամենատարածվածներ</button>
+    <button class="hdr" onclick="location.href='writer.html'">Պոետ</button>
+    <button class="hdr" onclick="location.href='xach.html'">ԽԱՉԲԱՌ</button>
+    <button class="hdr" onclick="location.href='dic.html'">Որոնում</button>
+    <button class="hdr" onclick="location.href='wordle.html'">WORDLE</button>
+    </div>
+    ${bdctx}
+    `;
 
+    bd.innerHTML = template;
+}
 
-bd.innerHTML = template;
+//<h1 style="font-family: 'Lucida Console', 'Courier New', monospace; color:rgb(255, 255, 255); text-align: center; font-size: 48px;">հանգեր.հայ</h1>
